8dd83cd0d2f3f5fe79fbcacf5f6524b6efe3e491
Shivshankar Sahu <shivsahu495@gmail.com>
2023-05-31 12:59:09 +0530

    ex 11

---
+++ ex_11/ex_11_drinkWaterRemainder.py
@@ -0,0 +1,10 @@
+import time
+
+def T_Time():
+    
+    while True:
+        time.sleep(300)
+        print("*******Drink Some water and Then Continue The Work..*******")
+
+T_Time()
+print("Interval")
