7d7ea0baf2219fa773287b07b3b478a3be51ac15
Shivshankar Sahu <shivsahu495@gmail.com>
2023-05-31 12:08:40 +0530

    modify news api

--- ex_10/ex_10_news_api.py
+++ ex_10/ex_10_news_api.py
@@ -4,7 +4,7 @@ import pprint
 # url = "https://newsapi.org/v2/top-headlines/sources?apiKey="
 category = input("Enter a category in which you want the newspaper like Business, general, technology... :) ")
 
-API_KEY = "03cf57f6ee9141a9871a4537c958a6cd"
+
 
 url = f"https://newsapi.org/v2/top-headlines/sources?category={category}&apiKey={API_KEY}"
 res = requests.get(url)
